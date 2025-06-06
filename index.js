97a5a680b8215706756acf82ada5ed04c624e9b3
Kevin Lpz <kevin.klv16@gmail.com>
2025-06-06 14:49:43 -0600

    Correction in the functions

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function suma (){
             num1 = document.getElementById("valor").value;
             num2 = document.getElementById("valor2").value;
             
-            let sumado = parseInt(num1) + parseInt(num2);
-            document.getElementById("resultado").innerHTML = "la suma de " + num1 + "+" + num2 + " = " + sumado;
+            let sum = parseInt(num1) + parseInt(num2);
+            document.getElementById("resultado").innerHTML = `La Suma de ${num1} + ${num2} = ${sum}`
 }
 
 function subs(){
@@ -13,7 +13,7 @@ function subs(){
             num2 = document.getElementById("valor2").value;
             
             let subsc = parseInt(num1) - parseInt(num2);
-            document.getElementById("resultado").innerHTML = "La resta de " + num1 + "-" + num2 + " = " + subsc;
+            document.getElementById("resultado").innerHTML = `La Resta de ${num1} - ${num2} = ${subsc}`
 }
 function multi (){
  let num1, num2;
@@ -21,15 +21,16 @@ function multi (){
             num2 = document.getElementById("valor2").value;
             
             let multipli = parseInt(num1) * parseInt(num2);
-            document.getElementById("resultado").innerHTML = "la multiplication de " + num1 + "*" + num2 + " = " + multipli;
+            document.getElementById("resultado").innerHTML = `La Multiplicacion de ${num1} * ${num2} = ${multipli}`
 }
 function div (){
  let num1, num2;
             num1 = document.getElementById("valor").value;
             num2 = document.getElementById("valor2").value;
             
-            let divi = parseInt(num1) / parseInt(num2);
-            document.getElementById("resultado").innerHTML = "la division de " + num1 + "/" + num2 + " = " + divi;
+           let divi = parseInt(num2) !== 0 ? parseInt(num1) / parseInt(num2) : "Error: Divide by zero.";
+           document.getElementById("resultado").innerHTML = `La división de ${num1} / ${num2} = ${divi}`;
+
 }
 
 
